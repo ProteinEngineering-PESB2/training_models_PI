d50035cf785a6f98f1189a5f3f08e4cfba46b0fe
dMedinaO <david.medina@cebib.cl>
2025-03-07 08:33:01 +0100

    updating demo training models

---
+++ src/utils/utilsLib.py
@@ -0,0 +1,32 @@
+from sklearn.preprocessing import MaxAbsScaler
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import make_scorer, mean_absolute_error, mean_squared_error, r2_score, root_mean_squared_error
+
+def applySplit(dataset, response, random_state=42, test_size=0.3):
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        dataset, 
+        response, 
+        random_state=random_state, 
+        test_size=test_size)
+    
+    return X_train, X_test, y_train, y_test
+
+def makeScoresForRegression():
+    scoring = {
+        'MAE': make_scorer(mean_absolute_error),
+        'MSE': make_scorer(mean_squared_error),
+        'R2': make_scorer(r2_score),
+        'RMSE' : make_scorer(root_mean_squared_error)
+    }
+
+    return scoring
+
+def scalerProcess(dataset):
+    scaler = MaxAbsScaler()
+    scaler.fit(dataset)
+    dataset_scaled = scaler.transform(dataset)
+
+    dataset_scaled = pd.DataFrame(data=dataset_scaled, columns=dataset.columns)
+    return dataset_scaled, scaler
